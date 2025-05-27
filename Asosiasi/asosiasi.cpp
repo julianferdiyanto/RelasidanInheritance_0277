cf74cfec8c138dcbca5c962432f4e6bd7bdf441c
julianferdiyanto <julianferdiyanto3@gmail.com>
2025-05-27 11:49:17 +0700

    menambahan class dokter dengan akses modifier public

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -17,3 +17,23 @@ class pasien{
         void tambahDokter(dokter*);
         void cetakDokter();
 };
+
+class dokter {
+    public:
+        string nama;
+        vector<pasien*> daftar_pasien;
+
+        dokter(string pNama): nama(pNama) {
+            cout << "Dokter \"" << nama << "\" ada\n";
+        }
+        ~dokter() {
+            cout << "Dokter \"" << nama << "\" tidak ada\n";
+        }
+
+        void tambahPasien(pasien*);
+        void cetakPasien();
+};
+
+void pasien::tambahDokter(dokter* pDokter) {
+    daftar_dokter.push_back(pDokter);
+}
